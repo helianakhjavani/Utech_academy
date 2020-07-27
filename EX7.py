abd7d59fa342f53e5fdbb33d3ca18694d690c675
helianakhjavani <68470629+helianakhjavani@users.noreply.github.com>
2020-07-27 02:06:49 -0700

    Add files via upload

--- EX7.py
+++ EX7.py
@@ -1,11 +1,21 @@
-def Fib(arg): 
+
+def Fib(arg):
     if arg<=0: 
         print("invalid") 
-    elif arg==1: 
+    elif arg==1:
         return 0
-    elif arg==2: 
+    elif arg==2:
         return 1
-    else: 
+    else:
         return Fib(arg-1)+Fib(arg-2)
+    
 
-print(Fib(7))
+def FibinList(arg):
+    L= []
+    for i in range(1,arg+1):
+        L.append(Fib(i))
+    return L
+
+
+print(FibinList(7))
+        
