29453a435bd95d6f787f96eabe743d2a708c7e3e
helianakhjavani <68470629+helianakhjavani@users.noreply.github.com>
2020-07-27 02:58:28 -0700

    Add files via upload

--- EX7.py
+++ EX7.py
@@ -1,21 +1,20 @@
+arr = []
+n=15
+def Fib(n):
 
-def Fib(arg):
-    if arg<=0: 
-        print("invalid") 
-    elif arg==1:
-        return 0
-    elif arg==2:
-        return 1
-    else:
-        return Fib(arg-1)+Fib(arg-2)
-    
+  if arr[n] != -1:
+    return arr[n]
+  if n == 0:
+    arr[0] = 0
+    return 0 #base case 1
+  elif n == 1:
+    arr[1] = 1
+    return 1  #base case 2
+  else:
+    arr[n] = Fib(n-1) + Fib(n-2)
+    return arr[n];
 
-def FibinList(arg):
-    L= []
-    for i in range(1,arg+1):
-        L.append(Fib(i))
-    return L
-
-
-
-        
+for i in range(n+1):
+    arr.append(-1)
+Fib(n)
+print(arr) 
